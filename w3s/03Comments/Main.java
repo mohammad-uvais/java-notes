9dc768bfaa538d94967e3daaa6db372e24369bb7
Uvais Mansoori <uvaismansoori@gmail.com>
2025-06-23 02:55:34 +0530

    Java Comments

---
+++ w3s/03Comments/Main.java
@@ -0,0 +1,23 @@
+public class Main {
+    public static void main(String[] args) {
+        // This is a single-line comment
+        System.out.println("This is a single-line comment example.");
+
+        System.out.println("This is an inline comment example."); // This is an inline comment
+
+        /*
+         * This is a multi-line comment.
+         * It can span multiple lines.
+         */
+        System.out.println("This is a multi-line comment example.");
+
+        /**
+         * This is a Javadoc comment.
+         * It is used to document classes, methods, and fields.
+         */
+        System.out.println("This is a Javadoc comment example.");
+
+        // Comments can be used to explain code or disable code execution
+        // System.out.println("This line is commented out and will not execute.");
+    }
+}
