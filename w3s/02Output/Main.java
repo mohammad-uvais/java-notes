d41f10376c3d089bb28eca740b6f014fba00bee6
Uvais Mansoori <uvaismansoori@gmail.com>
2025-06-23 02:47:09 +0530

    print numbers as an output

--- w3s/02Output/Main.java
+++ w3s/02Output/Main.java
@@ -1,6 +1,15 @@
 public class Main {
     public static void main(String[] args) {
+        // Print Text
         System.out.print("Hello, World! ");
         System.out.print("I will print on the same line.");
+
+        // Print Numbers
+        System.out.println(3);
+        System.out.println(358);
+        System.out.println(50000);
+
+        System.out.println(3 + 3);
+        System.out.println(2 * 5);
     }
 }
