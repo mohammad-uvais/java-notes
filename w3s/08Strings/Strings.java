1eeb692ab11c6cecc57cc9a0e3cc74be8d9436b1
Uvais Mansoori <uvaismansoori@gmail.com>
2025-07-05 00:35:30 +0530

    strings in java

---
+++ w3s/08Strings/Strings.java
@@ -0,0 +1,45 @@
+
+public class Strings {
+    public static void main(String[] args) {
+        // Strings in Java are a sequence of characters.
+        // Strings are objects that represent a sequence of characters.
+        // Strings are immutable, meaning once created, they cannot be changed.
+
+        // Creating a string
+        String greeting = "Hello, World!";
+        System.out.println(greeting); // Outputs: Hello, World!
+
+        // String length
+        int length = greeting.length();
+        System.out.println("Length of the string: " + length); // Outputs: 13
+
+        // Accessing characters in a string
+        char firstChar = greeting.charAt(0);
+        System.out.println("First character: " + firstChar); // Outputs: H
+
+        // Concatenating strings
+        String name = "Alice";
+        String welcomeMessage = greeting + " Welcome, " + name + "!";
+        System.out.println(welcomeMessage); // Outputs: Hello, World! Welcome, Alice!
+
+        // Converting to uppercase and lowercase
+        String upperCaseGreeting = greeting.toUpperCase();
+        String lowerCaseGreeting = greeting.toLowerCase();
+        System.out.println("Uppercase: " + upperCaseGreeting); // Outputs: HELLO, WORLD!
+        System.out.println("Lowercase: " + lowerCaseGreeting); // Outputs: hello, world!
+
+        // Finding a substring
+        int indexOfWorld = greeting.indexOf("World");
+        System.out.println("Index of 'World': " + indexOfWorld); // Outputs: 7
+
+        // Replacing characters in a string
+        String replacedGreeting = greeting.replace("World", "Java");
+        System.out.println(replacedGreeting); // Outputs: Hello, Java!
+
+        // Splitting a string
+        String[] words = greeting.split(", ");
+        for (String word : words) {
+            System.out.println(word); // Outputs each word on a new line
+        }
+    }
+}
